eda6f84dc7710cd61a2a02a879340309204abe44
nasirliturkan <tnesirli97@gmail.com>
2021-10-20 14:38:46 +0400

    feature/added table actions

--- src/Structure/index.tsx
+++ src/Structure/index.tsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   TreeList,
+  Editing,
   Column,
   ValidationRule,
   Lookup,
+  Button,
+  Sorting,
+  FilterRow,
+  HeaderFilter,
+  SearchPanel,
   StateStoring,
 } from "devextreme-react/tree-list";
 import { store, IStructureProps } from "../structure";
@@ -14,39 +20,76 @@ interface IProps {
 
 export const Structure: React.FunctionComponent<IProps> = (props: IProps) => {
   const { structuresData } = props;
+  useEffect(() => {
+    console.log("-------------------------", structuresData);
+    console.log("store", store);
+  }, [store]);
 
   const expandedRowKeys = [1];
+
+  const popupOptions = {
+    title: "Structure Info",
+    showTitle: true,
+    width: 700,
+  };
 
   const lookupData = {
     store: store,
   };
 
+  const onEditorPreparing = (e: any) => {
+    if (e.dataField === "Parent_ID" && e.row.data.id === 1) {
+      e.editorOptions.disabled = true;
+      e.editorOptions.value = null;
+    }
+  };
+  const onInitNewRow = (e: any) => {
+    e.data.parent_id = 1;
+  };
+
   return (
-    <div id="tree-list-demo">
+    <div id="tree-list">
       <TreeList
-        id="structure"
-        parentIdExpr="parent_id"
         dataSource={structuresData}
-        defaultExpandedRowKeys={expandedRowKeys}
+        columnAutoWidth={true}
         showRowLines={true}
         showBorders={true}
-        columnAutoWidth={true}
-        itemsExpr="items"
-        keyExpr="id"
-        dataStructure="tree"
+        defaultExpandedRowKeys={expandedRowKeys}
+        keyExpr="ID"
+        parentIdExpr="Parent_ID"
+        onEditorPreparing={onEditorPreparing}
+        onInitNewRow={onInitNewRow}
+        selectedRowKeys={[1]}
       >
         <StateStoring
           enabled={true}
           type="localStorage"
           storageKey="structure"
         />
-        <Column dataField="name" caption="Name">
-          <Lookup dataSource={lookupData} valueExpr="name" displayExpr="name" />
+        <FilterRow visible={true} />
+        <HeaderFilter visible={true} />
+        <SearchPanel visible={true} />
+        <Sorting mode="multiple" />
+        <Editing
+          allowUpdating={true}
+          allowDeleting={true}
+          allowAdding={true}
+          popup={popupOptions}
+          mode="popup"
+        />
+        <Column dataField="Name">
           <ValidationRule type="required" />
         </Column>
-        <Column dataField="status" caption="Status">
-          <Lookup dataSource={lookupData} displayExpr="status" valueExpr="status" />
+        <Column dataField="Status" caption="Status">
           <ValidationRule type="required" />
+        </Column>
+        <Column visible={false} dataField="Parent_ID" caption="Parent">
+          <Lookup dataSource={lookupData} valueExpr="ID" displayExpr="Name" />
+          <ValidationRule type="required" />
+        </Column>
+        <Column type="buttons">
+          <Button name="edit" />
+          <Button name="delete" />
         </Column>
       </TreeList>
     </div>
